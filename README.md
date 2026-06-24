8280c926a9e7290a7f239c0b73f1e22698b66efa
Reggie Bigornia <reggietheroman@gmail.com>
2026-06-24 11:40:49 +0800

    [Feature][Reggie] Add daisyui package and markdown-it package. Add daisyui to css plugins list.

---
+++ README.md
@@ -0,0 +1,10 @@
+# 11ty ReggieTheRoman
+
+My personal website built using 11ty. It uses [Liquid](https://liquidjs.com/) for templating and [Tailwind](https://tailwindcss.com/) for css.
+
+- Liquid template component files belong in `src/_layouts`
+- CSS files belong in `src/assets/css`
+- JS files belong in `src/assets/js`
+
+A lot of architecture and examples are a callback to my love of [Ember.js](https://emberjs.com/)
+
---
+++ eleventy.config.js
@@ -0,0 +1,51 @@
+import Image from"@11ty/eleventy-img";
+import path from "path";
+// import handlebars from "handlebars";
+import handlebarsPlugin from "@11ty/eleventy-plugin-handlebars";
+
+export default async function(eleventyConfig) {
+  // add plugin for handlebars
+  eleventyConfig.addPlugin(handlebarsPlugin);
+  // eleventyConfig.addPlugin(handlebarsPlugin, {
+	// 	// Override the `ejs` library instance
+	// 	eleventyLibraryOverride: handlebars,
+	// });
+
+  // Async Shortcode for Optimized Images
+  eleventyConfig.addAsyncShortcode("optimizedImage", async function(src, alt, widths = [300, 600, 900]) {
+    let fullSrc = path.join(__dirname, "src", src);
+    
+    let metadata = await Image(fullSrc, {
+      widths: widths,
+      formats: ["avif", "webp", "jpeg"],
+      outputDir: "./dist/img/",
+      urlPath: "/img/"
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes: "(min-width: 30em) 50vw, 100vw",
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
+  // Pass through static assets directly to dist/
+  eleventyConfig.addPassthroughCopy("src/assets/js");
+  eleventyConfig.addPassthroughCopy("src/assets/fonts");
+  
+  // Return your configuration object
+  return {
+    dir: {
+      input: "src",          // Source files
+      output: "dist",        // Built static site
+      includes: "_includes", // Partials and shortcodes (relative to input)
+      layouts: "_layouts"    // Layout files (relative to input)
+    },
+    markdownTemplateEngine: "hbs",
+    // htmlTemplateEngine: "hbs"
+  };
+};
+
